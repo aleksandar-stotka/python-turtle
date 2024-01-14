f9de96f6737fe6bb44698ee649c294f6ef1f998f
aleksandar-stotka <aangelevski6@gmail.com>
2024-01-14 14:17:07 +0100

    create_func_validate

--- scoreboard.py
+++ scoreboard.py
@@ -1,23 +1,29 @@
 FONT = ("Courier", 24, "normal")
 
 night_of_coding = 33
-
+user_input = input("how many")
 def day_of_units(day):
-    if day > 0:
-     return f"i need {day} day of coding and {night_of_coding} night of coding"
-    else:
-        return "not valid number"
+    return f"i need {day} day of coding and {night_of_coding} night of coding"
 
-user_input = input("how many day of coding and night you need\n")
+def validate_user_input():
+    try:
+        user_number = int(user_input)
+        if user_number > 0:
+            calculate_value = day_of_units(user_number)
+            print(calculate_value)
+        elif user_number == 0:
+            print("Please enter a positive number.")
+    except ValueError:
+        print("Your input is not a valid number.")
+        
 
-user_number = int(user_input)
+
+validate_user_input()
 
 
 
 
-calculated_value = day_of_units(user_number)
 
-print(calculated_value)
 
 
 
